12e82ca9e68d6ed4ffc738b55c774d22a29d872c
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2025-02-18 08:53:10 +0100

    feat: setup credentials auth

--- internal/models/user.go
+++ internal/models/user.go
@@ -10,7 +10,7 @@ type User struct {
 	ID        uint           `gorm:"primarykey" json:"id"`
 	Email     string         `gorm:"uniqueIndex;not null" json:"email"`
 	Password  *string        `json:"-"`
-	Provider  string         `gorm:"not null;default:'google'" json:"provider"`
+	Provider  string         `gorm:"not null;default:'credentials'" json:"provider"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
