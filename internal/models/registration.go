bc12dc588d9030180a39b28af373c5e0fd5a949c
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2025-03-24 19:41:03 +0100

    feat: add additional fields to registration model

--- internal/models/registration.go
+++ internal/models/registration.go
@@ -6,14 +6,24 @@ import (
 	"gorm.io/gorm"
 )
 
+type RegistrationStatus string
+
+const (
+	RegistrationStatusPending  RegistrationStatus = "pending"
+	RegistrationStatusApproved RegistrationStatus = "approved"
+	RegistrationStatusRejected RegistrationStatus = "rejected"
+)
+
 type Registration struct {
-	ID        uint           `gorm:"primarykey" json:"id"`
-	EventID   uint           `gorm:"not null" json:"event_id"`
-	Event     Event          `gorm:"foreignKey:EventID" json:"event"`
-	UserID    uint           `gorm:"not null" json:"user_id"`
-	User      User           `gorm:"foreignKey:UserID" json:"user"`
-	Status    string         `gorm:"not null" json:"status"` // pending, approved, rejected
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+	ID                  uint               `gorm:"primarykey" json:"id"`
+	EventID             uint               `gorm:"not null" json:"event_id"`
+	Event               Event              `gorm:"foreignKey:EventID" json:"event"`
+	UserID              uint               `gorm:"not null" json:"user_id"`
+	User                User               `gorm:"foreignKey:UserID" json:"user"`
+	Status              RegistrationStatus `gorm:"not null" json:"status"`
+	Attended            bool               `gorm:"not null" json:"attended"`
+	DiateryRestrictions string             `json:"diatery_restrictions"`
+	CreatedAt           time.Time          `json:"created_at"`
+	UpdatedAt           time.Time          `json:"updated_at"`
+	DeletedAt           gorm.DeletedAt     `gorm:"index" json:"-"`
 }
