57d96cae1dbb4432a303e43fa21f3acce3b67542
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2025-03-24 19:22:36 +0100

    feat: change Event model to use more data fields

--- internal/models/event.go
+++ internal/models/event.go
@@ -6,15 +6,41 @@ import (
 	"gorm.io/gorm"
 )
 
+type EventType string
+
+const (
+	EventTypeLecture    EventType = "lecture"
+	EventTypeWorkshop   EventType = "webinar"
+	EventTypeSeminar    EventType = "hackathon"
+	EventTypeConference EventType = "workshop"
+	EventTypeJobFair    EventType = "job fair"
+	EventTypeOther      EventType = "other"
+	// TODO: add more event types
+)
+
+type RegistrationMethod string
+
+const (
+	RegistrationMethodWebsite RegistrationMethod = "website"
+	RegistrationMethodLink    RegistrationMethod = "link"
+	// TODO: add more registration methods
+)
+
 type Event struct {
-	ID          uint           `gorm:"primarykey" json:"id"`
-	Title       string         `gorm:"not null" json:"title"`
-	Description string         `json:"description"`
-	StartDate   time.Time      `json:"start_date"`
-	EndDate     time.Time      `json:"end_date"`
-	CreatedBy   uint           `json:"created_by"`
-	User        User           `gorm:"foreignKey:CreatedBy" json:"user"`
-	CreatedAt   time.Time      `json:"created_at"`
-	UpdatedAt   time.Time      `json:"updated_at"`
-	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-"`
+	ID                 uint               `gorm:"primarykey" json:"id"`
+	Title              string             `gorm:"not null" json:"title"`
+	Description        string             `json:"description"`
+	RegistrationMethod RegistrationMethod `json:"registration_method"`
+	ICSFileEndpoint    string             `json:"ics_file_endpoint"`
+	Location           string             `json:"location"`
+	Image              string             `json:"image"`
+	RegistrationMax    int                `json:"registration_max"`
+	TypeOfEvent        EventType          `json:"type_of_event"`
+	StartDate          time.Time          `json:"start_date"`
+	EndDate            time.Time          `json:"end_date"`
+	CreatedBy          uint               `json:"created_by"`
+	User               User               `gorm:"foreignKey:CreatedBy" json:"user"`
+	CreatedAt          time.Time          `json:"created_at"`
+	UpdatedAt          time.Time          `json:"updated_at"`
+	DeletedAt          gorm.DeletedAt     `gorm:"index" json:"-"`
 }
