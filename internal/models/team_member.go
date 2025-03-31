818aff467c73e4d66a09a53cbc0f677b28f9c272
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2025-03-31 14:39:34 +0200

    feat: update team member model to meet the requirement specification

--- internal/models/team_member.go
+++ internal/models/team_member.go
@@ -2,16 +2,24 @@ package models
 
 import (
 	"time"
+)
 
-	"gorm.io/gorm"
+type TeamMemberRole string
+
+const (
+	TeamMemberRoleAdmin  TeamMemberRole = "admin"
+	TeamMemberRoleMember TeamMemberRole = "member"
 )
 
 type TeamMember struct {
-	ID        uint           `gorm:"primarykey" json:"id"`
-	UserID    uint           `gorm:"not null" json:"user_id"`
-	User      User           `gorm:"foreignKey:UserID" json:"user"`
-	Role      string         `gorm:"not null" json:"role"` // admin, member
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+	ID           uint           `gorm:"primarykey" json:"id"`
+	UserID       uint           `gorm:"not null" json:"user_id"`
+	User         User           `gorm:"foreignKey:UserID" json:"user"`
+	Title        string         `gorm:"not null" json:"title"`
+	Role         TeamMemberRole `gorm:"not null" json:"role"`
+	GitHubLink   string         `json:"github_link,omitempty"`
+	LinkedInLink string         `json:"linkedin_link,omitempty"`
+	AcademicYear int            `gorm:"not null" json:"academic_year"`
+	CreatedAt    time.Time      `json:"created_at"`
+	UpdatedAt    time.Time      `json:"updated_at"`
 }
