011cbb43436ac5d2e91f71bf6b9a7ab87f619f89
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2025-04-02 13:34:42 +0200

    feat: add Alumni model

---
+++ internal/models/alumni.go
@@ -0,0 +1,10 @@
+package models
+
+type Alumni struct {
+	ID              uint   `gorm:"primarykey" json:"id"`
+	Name            string `gorm:"not null" json:"name"`
+	Title           string `gorm:"not null" json:"kthaist_title"`
+	Period          string `gorm:"not null" json:"period"`
+	Description     string `gorm:"not null" json:"description"`
+	CurrentPosition string `gorm:"not null" json:"current_position"`
+}
