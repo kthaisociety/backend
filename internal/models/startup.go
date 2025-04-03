ace1c5bcc340e132d9f2475867c9978b1e688f32
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2025-04-03 20:32:11 +0200

    fix: fix weird name of var of startup model

--- internal/models/startup.go
+++ internal/models/startup.go
@@ -1,9 +1,9 @@
 package models
 
 type Startup struct {
-	ID            uint   `gorm:"primarykey" json:"id"`
-	Name          string `gorm:"not null" json:"name"`
-	Description   string `gorm:"not null" json:"description"`
-	Image         string `gorm:"not null" json:"image"`
-	GroupedByYear string `gorm:"not null" json:"grouped_by_year"`
+	ID          uint   `gorm:"primarykey" json:"id"`
+	Name        string `gorm:"not null" json:"name"`
+	Description string `gorm:"not null" json:"description"`
+	Image       string `gorm:"not null" json:"image"`
+	Year        string `gorm:"not null" json:"year"`
 }
