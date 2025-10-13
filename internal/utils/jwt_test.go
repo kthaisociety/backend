ce6a8b335dadb98f1e43bb7b9641c998593c7ac7
Vivienne Curewitz <jaycie.curewitz@gmail.com>
2025-10-13 10:57:08 +0200

    removed file dependencies from test

--- internal/utils/jwt_test.go
+++ internal/utils/jwt_test.go
@@ -1,22 +1,14 @@
 package utils
 
 import (
-	"bufio"
 	"log"
-	"os"
 	"testing"
 
 	"github.com/google/uuid"
 )
 
 func TestParseJWT(t *testing.T) {
-	filename := "google.jwt"
-	f, _ := os.Open(filename)
-	reader := bufio.NewReader(f)
-	test_jwt, isPrefix, _ := reader.ReadLine()
-	if isPrefix {
-		log.Println("Long JWT")
-	}
+	test_jwt := "eyJhbGciOiJSUzI1NiIsImtpZCI6ImM4YWI3MTUzMDk3MmJiYTIwYjQ5Zjc4YTA5Yzk4NTJjNDNmZjkxMTgiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI2ODc4OTA1MzA1MS1lcDQ4dDVxbGNrNThnYW0xaGlkNDFnYmJvaHYzbHIyNi5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF1ZCI6IjY4Nzg5MDUzMDUxLWVwNDh0NXFsY2s1OGdhbTFoaWQ0MWdiYm9odjNscjI2LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTE2MDY5ODg0OTYzMDA3NTM0NTU3IiwiaGQiOiJrdGhhaXMuY29tIiwiZW1haWwiOiJ2aXZpZW5uZUBrdGhhaXMuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiJULUlrQXdROHVzRXFFLUFhOU5oT2JRIiwibmFtZSI6IlZpdmllbm5lIEN1cmV3aXR6IiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FDZzhvY0t1bURQRTZoWW5ERzY4bkt0Nlp1Q3JtbGtMUEJPbXpxQVBValp3bFJGTWpYU3ZBZz1zOTYtYyIsImdpdmVuX25hbWUiOiJWaXZpZW5uZSIsImZhbWlseV9uYW1lIjoiQ3VyZXdpdHoiLCJpYXQiOjE3NTk5NDY2ODEsImV4cCI6MTc1OTk1MDI4MX0.afkVhYSvqRvPjnkfNOmAkiS9xo-mwFKy2cgzR2xndHRE1XE8DrVuHZflIdG3PMknW4-R6tfhHZZqLH7OXJGgg9AKbQk1C_pl10rk_5hG5vm2b2Es_r32M6ZyUEHIQqBeT5AQ4cSTPmduQuTGwI9Ku0dv12xJQ25FJXmzktHe0ijMNBeIeScfhXZqlQSs6Dgutd2n9YaJCJmcxFbFihN8fyhXEXWm6F4BdjtE6bY2oPUKTxrlbelkz-B0z72Y6utZrBnDMEFmfpARISmUynaCM5zVA4DX4IeVtTGRPuAG-Y2T0jAM-s6wn4nGXZYbuRDW6Q7qVz-gX_1Pv9e63NZJmQ"
 	_, err := ParseJWT(string(test_jwt))
 	if err != nil {
 		t.Errorf("Error Parsing JWT %v\n", err)
