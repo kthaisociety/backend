a0768f9a9f99e06801e2a8c611222f939d97378b
Vivienne Curewitz <jaycie.curewitz@gmail.com>
2025-11-05 17:09:52 +0100

    blob data passes tests

--- internal/models/blob_data.go
+++ internal/models/blob_data.go
@@ -1,29 +1,44 @@
 package models
 
 import (
+	"backend/internal/utils"
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
+// BlobId is unique to the blob, and the index it is stored at in R2
+// AssociationId is the thing it is associated with, such as an event, project, or user
+// Name - just a string, may be useful
+// FType - what is it? .png, .jpeg, .zip, etc
 type BlobData struct {
 	gorm.Model
-	UserId uuid.UUID `gorm:"uniqueIndex" json:"user_id"`
-	Uri    string    `json:"uri"`
+	BlobId        uuid.UUID `gorm:"uniqueIndex" json:"blob_id"`
+	AssociationId uuid.UUID `json:"association_id"`
+	Name          string    `json:"name"`
+	FType         string    `json:"ftype"`
 }
 
-func (bd BlobData) getData() ([]byte, error) {
+// Get the bd from the database, then call this to get a bytearray that represents the data.
+func (bd BlobData) GetData(r2 utils.R2Client) ([]byte, error) {
 	///fetch bd.uri from bucket
-	//return
-	return []byte{}, nil
+	return r2.GetObject(bd.BlobId.String())
 }
 
-func NewBlobData(Blob []byte, db gorm.DB) (BlobData, error) {
-	// push data to R2
+// input name, type, and association id (who it belongs to), actual data as a byte array, the db client and the r2 client
+func NewBlobData(name string, ftype string, association_id uuid.UUID, blob []byte, db *gorm.DB, r2 utils.R2Client) (BlobData, error) {
 	// create struct
 	id, _ := uuid.NewUUID()
 	bd := BlobData{
-		UserId: id,
-		Uri:    "asdfasdf",
+		BlobId:        id,
+		AssociationId: association_id,
+		Name:          name,
+		FType:         ftype,
+	}
+	// push data to R2
+	err := r2.PutObject(id.String(), blob)
+	if err != nil {
+		return bd, err
 	}
 	// push to postgresql
 	if err := db.Create(&bd).Error; err != nil {
